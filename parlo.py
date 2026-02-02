3a6bb54d7e32411e9384a19296685a623c80ea14
marcobetto <marco4shape@gmail.com>
2026-02-02 11:14:03 +0100

    Update parlo.py

--- parlo.py
+++ parlo.py
@@ -1,10 +1,12 @@
-import socket
-s = socket.socket()
-s.settimeout(2)
-indirizzo = "127.0.0.1"
-porta = 6364
-s.connect((indirizzo, porta))
-s.sendall(b'che confusione, sara perche programmo')
-risposta = s.recv(1024)
-print(risposta.decode())
-s.close
+    import socket
+    import sys
+    messaggio = sys.argv[1]
+    s = socket.socket()
+    s.settimeout(2)
+    indirizzo = "127.0.0.1"
+    porta = 6364
+    s.connect((indirizzo, porta))
+    s.sendall(b'che confusione, sara perche programmo')
+    risposta = s.recv(1024)
+    print(risposta.decode())
+    s.close()
