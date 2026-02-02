a0bb9d01b4712afd0d609d6aee6d25b27478bb61
marcobetto <marco4shape@gmail.com>
2026-02-02 11:22:19 +0100

    Update parlo.py

--- parlo.py
+++ parlo.py
@@ -6,7 +6,7 @@
     indirizzo = "127.0.0.1"
     porta = 6364
     s.connect((indirizzo, porta))
-    s.sendall(b'che confusione, sara perche programmo')
+    s.sendall(messaggio.encode()) #(b'che confusione, sara perche programmo')
     risposta = s.recv(1024)
     print(risposta.decode())
     s.close()
