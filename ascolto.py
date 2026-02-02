2ec57ade701eb65c3d4254d01b75d1cc86a6ea9f
marcobetto <marco4shape@gmail.com>
2026-02-02 11:13:35 +0100

    Update ascolto.py

--- ascolto.py
+++ ascolto.py
@@ -1,26 +1,30 @@
-import socket
-server_socket = socket.socket()
-host = '127.0.0.1'
-port = 6364
-server_socket.bind((host, port))
-server_socket.listen(1)
-print(f"server listening on {host}:{port}") #f consente di incastonare le graffe
-conn, addr = server_socket.accept() #accept è un metodo, ascolta la porta indicata sopra
-print(f"connected by {addr}")
-#apertura file in scrittura, la fx open non ha bisogno di import e la w apre il file in modalità scrittura
-f = open("logfile.txt", "w")
-try:
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        richiesta = data.decode()
-        risposta = f"ho ricevuto il tuo messaggio: {richiesta}"  
-        #print(risposta)
-        f.write(risposta)
-        conn.sendall(risposta.encode())
-finally:
-    #chiudere funzione f
-    f.close()
-    conn.close()
+    import socket
+    server_socket = socket.socket()
+    host = '127.0.0.1'
+    port = 6364
+    server_socket.bind((host, port))
+    server_socket.listen(1)
+    f = open('logfile.txt', 'w') #apertura file in scrittura, la fx open non ha bisogno di import e la w apre il file in modalità scrittura
+    f.write(f"server listening on {host}:{port}\n")
+    for i in range (6):
+        conn, addr = server_socket.accept() #accept è un metodo, ascolta la porta indicata sopra
+        f.write(f"connected by {addr}\n")
+        try:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                richiesta = data.decode()
+                if richiesta == 'SHUTDOWN':
+                    break
+                risposta = f"ho ricevuto il tuo messaggio: {richiesta}\n"  
+            #print(risposta)
+                f.write(risposta)
+                conn.sendall(risposta.encode())
+            
+            if richiesta == 'SHUTDOWN':
+                break
+        finally:
+            conn.close() 
+    f.close()    #chiudere funzione f
     server_socket.close()
